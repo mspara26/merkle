99a09d58a31c6cf034bb8ad59942d910b9d452a5
mspara26 <161856705+mspara26@users.noreply.github.com>
2025-05-14 13:22:37 -0400

    Update aem.js

--- scripts/aem.js
+++ scripts/aem.js
@@ -163,30 +163,6 @@ function init() {
 }
 
 /**
- * Sanitizes a string for use as class name.
- * @param {string} name The unsanitized string
- * @returns {string} The class name
- */
-function toClassName(name) {
-  return typeof name === 'string'
-    ? name
-      .toLowerCase()
-      .replace(/[^0-9a-z]/gi, '-')
-      .replace(/-+/g, '-')
-      .replace(/^-|-$/g, '')
-    : '';
-}
-
-/**
- * Sanitizes a string for use as a js property name.
- * @param {string} name The unsanitized string
- * @returns {string} The camelCased name
- */
-function toCamelCase(name) {
-  return toClassName(name).replace(/-([a-z])/g, (g) => g[1].toUpperCase());
-}
-
-/**
  * Extracts the config from a block.
  * @param {Element} block The block element
  * @returns {object} The block config
